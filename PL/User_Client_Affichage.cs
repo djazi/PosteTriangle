4c0b0b8a110bc7846b841545282f3dfcc1848ec8
djazi <52473469+djazi@users.noreply.github.com>
2020-12-03 03:04:25 +0100

    Fin 2nd day  creation d'interface de performance vide

---
+++ PL/User_Client_Affichage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PosteTriangle.PL
+{
+    public partial class User_Client_Affichage : UserControl
+    {
+        public User_Client_Affichage()
+        {
+            InitializeComponent();
+        }
+    }
+}
